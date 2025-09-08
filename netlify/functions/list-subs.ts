f0caddae9ddeaf4f2b97474a17523b9f363339e6
federemy <federemy@gmail.com>
2025-09-07 23:43:57 -0300

    notif

--- netlify/functions/list-subs.ts
+++ netlify/functions/list-subs.ts
@@ -2,22 +2,16 @@
 import type { Handler } from "@netlify/functions";
 import { getBlobStore, getList } from "./_store";
 
-type Sub = { endpoint: string; keys?: { p256dh?: string; auth?: string } };
-
-const handler: Handler = async () => {
+export const handler: Handler = async () => {
   try {
-    // lista (subs/list)
-    const list = (await getList<Sub[]>("subs", "list")) || [];
-
-    // blobs individuales (subs/*) por si hay restos
+    const list = (await getList<any[]>("subs", "list")) || [];
     const store = getBlobStore("subs");
     const { blobs } = await store.list();
-    const blobSubs: Sub[] = [];
+    const blobSubs: any[] = [];
     for (const b of blobs) {
       const s = await store.get(b.key, { type: "json" });
-      if (s?.endpoint) blobSubs.push(s as Sub);
+      if (s?.endpoint) blobSubs.push(s);
     }
-
     return {
       statusCode: 200,
       headers: { "content-type": "application/json" },
@@ -25,7 +19,6 @@ const handler: Handler = async () => {
         ok: true,
         listCount: list.length,
         blobCount: blobSubs.length,
-        // para no imprimir datos sensibles, mostramos sólo endpoints
         list: list.map((s) => ({ endpoint: s.endpoint })),
         blobs: blobSubs.map((s) => ({ endpoint: s.endpoint })),
       }),
@@ -38,5 +31,3 @@ const handler: Handler = async () => {
     };
   }
 };
-
-export default handler;
